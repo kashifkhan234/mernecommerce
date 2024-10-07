f2dd326880069005af5da63021e520178f4a1fda
kashifkhan234 <kashifkhan20977@gmail.com>
2024-10-07 16:07:59 +0530

    db js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDb = require('../backend/config/db.js')
+const connectDb = require('../backend/config/dbj')
 const router = require('./routes');
 const cookieParser = require('cookie-parser');
 
